2bde69b7fe763d5ccc833664a3d1a0bcb0e9834c
Phelan Lewis <phelanlewis@gmail.com>
2017-01-18 23:01:30 +0000

    Added edit and delete feature

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -20,6 +21,21 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//Deletes the url id
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect(302, "/urls/");
+})
+//Updates the urls
+app.post("/urls/:id/update", (req, res) => {
+  let newLongURL = req.body["newLongURL"];
+  console.log(newLongURL);
+  urlDatabase[req.params.id] = newLongURL;
+  console.log(urlDatabase);
+  res.redirect(302, "/urls/");
+})
+
+//Posts new urls
 app.post("/urls", (req, res) => {
   let longURL = req.body["longURL"];
   let shortURL = generateRandomString();
@@ -27,21 +43,24 @@ app.post("/urls", (req, res) => {
   console.log(shortURL);
   urlDatabase[shortURL] = longURL;
   console.log(urlDatabase);
-  res.redirect(302, "http://localhost:8080/urls/"+shortURL);
+  res.redirect(302, "/urls/");
 });
 
+//Index for the app
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//Shows specific url page
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+//Some sort of redirect
 app.get("/u/:shortURL", (req, res) => {
- let longURL = urlDatabase[req.params.shortURL]; 
+ let longURL = urlDatabase[req.params.shortURL];
  if (longURL != null) {
  res.redirect(longURL);
  } else {
