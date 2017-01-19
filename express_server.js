1578d0c7e2c6a58d040616487b48780c79ddfb40
Phelan Lewis <phelanlewis@gmail.com>
2017-01-19 00:23:29 +0000

    Cookies are delicious

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 
@@ -8,24 +9,39 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+const username = {
+
+};
+
 function generateRandomString() {
   let random = Math.random().toString(36).substring(5, 11);
   return random;
 }
 
 app.set("view engine", "ejs");
-
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+//Display the newly added URL on their own page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+//Login Users
+app.post("/urls/login", (req, res) => {
+  let username = req.body.username
+  res.cookie("username", username)
+  res.redirect(302,"http://localhost:8080/urls/");
 });
 
 //Deletes the url id
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect(302, "/urls/");
-})
+});
+
 //Updates the urls
 app.post("/urls/:id/update", (req, res) => {
   let newLongURL = req.body["newLongURL"];
@@ -33,22 +49,24 @@ app.post("/urls/:id/update", (req, res) => {
   urlDatabase[req.params.id] = newLongURL;
   console.log(urlDatabase);
   res.redirect(302, "/urls/");
-})
+});
 
 //Posts new urls
 app.post("/urls", (req, res) => {
   let longURL = req.body["longURL"];
   let shortURL = generateRandomString();
-  console.log(longURL);
-  console.log(shortURL);
+      console.log(longURL);
+      console.log(shortURL);
   urlDatabase[shortURL] = longURL;
-  console.log(urlDatabase);
+      console.log(urlDatabase);
+      console.log('Cookies: ', req.cookies);
+      console.log('Signed Cookies: ', req.signedCookies);
   res.redirect(302, "/urls/");
 });
 
 //Index for the app
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {  username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
